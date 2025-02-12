f1357ab99cb616011473908c4867fc1a5b40dd3f
Sunghum Paik (Brian) <zerosial@gmail.com>
2025-02-12 16:17:38 +0900

    feat: llm chain을 위한 사전 함수 지정

--- src/llmChain/llmChain.ts
+++ src/llmChain/llmChain.ts
@@ -1,15 +1,30 @@
 import { PromptTemplate } from "@langchain/core/prompts";
+import { OpenAI } from "@langchain/openai";
+import dotenv from "dotenv";
+import { StringOutputParser } from "@langchain/core/output_parsers";
 
-const promptTemplate = new PromptTemplate({
-  template:
-    "{course}에서 특정 {role}가 공부하는것의 중요성을 {wordLimit} 단어 이하로 설명하세요",
-  inputVariables: ["course", "role", "wordLimit"],
-});
+dotenv.config();
 
-const formattedPrompt = await promptTemplate.format({
-  course: "AI 생성",
-  role: "JavaScript 개발자",
-  wordLimit: "100",
-});
+async function personalizedPitch(
+  course: string,
+  role: string,
+  wordLimit: number
+) {
+  const promptTemplate = new PromptTemplate({
+    template:
+      "{course}에서 특정 {role}가 공부하는것의 중요성을 {wordLimit} 단어 이하로 설명하세요",
+    inputVariables: ["course", "role", "wordLimit"],
+  });
 
-console.log("Formatted Prompt: ", formattedPrompt);
+  const formattedPrompt = await promptTemplate.format({
+    course,
+    role,
+    wordLimit,
+  });
+
+  const llm = new OpenAI({
+    modelName: "gpt-3.5-turbo-instruct",
+  });
+
+  const outputParser = new StringOutputParser();
+}
